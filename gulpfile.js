222e116f8815cd75dc05a524981d656ede6b948b
CodeAkinori <yuukko404@gmail.com>
2025-08-27 22:27:19 -0300

    modificando dependências

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-dart-sass');
 const imagemin = require ('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
